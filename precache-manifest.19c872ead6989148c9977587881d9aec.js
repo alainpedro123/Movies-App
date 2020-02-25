03a783c10d337c1ed5348946a009e46fda98409a
Alain Afonso <alainpedro123@gmail.com>
2020-02-25 12:47:18 +0100

    Updates

---
+++ precache-manifest.19c872ead6989148c9977587881d9aec.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5d1e2da54da0b930e76616822e45104",
+    "url": "/Movies-App/index.html"
+  },
+  {
+    "revision": "f30da788faeff16fe241",
+    "url": "/Movies-App/static/css/2.b7670477.chunk.css"
+  },
+  {
+    "revision": "3d3ddf5564b0719e52b8",
+    "url": "/Movies-App/static/css/main.b70bc6cc.chunk.css"
+  },
+  {
+    "revision": "f30da788faeff16fe241",
+    "url": "/Movies-App/static/js/2.1374f69a.chunk.js"
+  },
+  {
+    "revision": "3d3ddf5564b0719e52b8",
+    "url": "/Movies-App/static/js/main.640c8dab.chunk.js"
+  },
+  {
+    "revision": "c2fb0a874df1335d315c",
+    "url": "/Movies-App/static/js/runtime-main.33767395.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Movies-App/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Movies-App/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Movies-App/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Movies-App/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Movies-App/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
